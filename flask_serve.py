9eba9e917392dc79efa264322756ff0d9ebd38a9
Robs <57124267+azure1990@users.noreply.github.com>
2022-06-08 20:10:58 -0600

    Update flask_serve.py

--- flask_serve.py
+++ flask_serve.py
@@ -1,1 +1,12 @@
 
+from flask import Flask, request, render_template
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+@app.route('/')
+def hello():
+  return "Holix mundo"
+
+app.run()
