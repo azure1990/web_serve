82c5d976f3e4896fa36be894f7cf9b962c317769
Robs <57124267+azure1990@users.noreply.github.com>
2022-06-14 23:07:29 -0600

    Update http_server.py

--- def_edtion/http_server.py
+++ def_edtion/http_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 
 d_app = Flask(__name__)
 print(__name__)
@@ -29,11 +29,18 @@ def contact():
 def html_page(page_name):
   return render_template('page_name')
 
+def write_to_file(data):
+  with open("database.txt", mode='a') as database:
+    email = data['email']
+    subject = data['subject']
+    message = data['message']
+    file = database.write(f'\n{email},{subject},{message}')
+
 @d_app('/submit_form', methods['POST', 'GET'])
 def submit_form():
   if request.method=='POST':
     data = request.form.to_dict()
-    print(data)
-    return "Form submitted"
+    write_to_file(data)
+    return redirect('/thankyou.html')
   else:
     return "Something went wrong"
