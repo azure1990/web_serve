aced796511258220cef9a87ce5510fba1dc2e878
Robs <robertocruz.cr@gmail.com>
2022-06-16 00:35:20 -0600

    Lates change on laptop

--- def_edtion/http_server.py
+++ def_edtion/http_server.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, url_for, request, redirect
 
 d_app = Flask(__name__)
-print(__name__)
 
-@d_app('/')
+
+@d_app.route('/')
 def my_home():
   return render_template('index.html')
 """
@@ -25,9 +25,9 @@ def contact():
 """
 
 
-@d_app('/<string:page_name>')
+@d_app.route('/<string:page_name>')
 def html_page(page_name):
-  return render_template('page_name')
+  return render_template(page_name)
 
 def write_to_file(data):
   with open("database.txt", mode='a') as database:
@@ -36,7 +36,7 @@ def write_to_file(data):
     message = data['message']
     file = database.write(f'\n{email},{subject},{message}')
 
-@d_app('/submit_form', methods['POST', 'GET'])
+@d_app.route('/submit_form', methods = ['POST', 'GET'])
 def submit_form():
   if request.method=='POST':
     data = request.form.to_dict()
