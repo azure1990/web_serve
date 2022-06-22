f548c6fa1808c9de37a9b99f2dbad6ca4637cc5d
Robs <57124267+azure1990@users.noreply.github.com>
2022-06-21 23:16:07 -0600

    Update alquimia.py

--- test_env2/alquimia.py
+++ test_env2/alquimia.py
@@ -36,29 +36,61 @@ Aqui procedemos a agregar lo que serian los atributos de la clase, que son en si
     self.author = author
     self.number_pgs = number_pgs
   
+"""
+Creamos la tabla usando esta funcion del objeto db:
+db.create_all()
+  
+Usamos tambien las siguientes funciones de acuerdo a la accion:
+  
+Para insertar los registros primero
+db.session.add(objeto del model (un objeto libro en esta app))
+y luego
+db.session.commit(()
+
+Para eliminar los registros
+db.session.delete(objeto del model (un objeto libro en esta app))
+  
+Para llamar todos los registros de libro (libros.qury.all() en nuestro ejemplo)
+model.query.all() 
+  
+Pdemos igual usar filtros, por ejemplo
+libros.query.filter_by(author = 'Elvis Poin').all()
+"""
+  
+"""
+Ahora a continuacion pasamos a disenar las views, primero la de home view que mostrara todos los libros
+"""
+  
+@app_db.route('/')
+def show_all():
+  return render_template('show_all.html', libros = libros.query.all())
+
+"""
+Ahora procedemos a agregar la view para agregar los poderosos libros
+esta view/ruta funcionara con dos metodos, get y post, y hara algo distinto dependiendo de cual metodo sea usado para generar
+la request
+"""
+
+@app_db.route('/new', method=['GET', 'POST'])
+def new():
   """
-  Creamos la tabla usando esta funcion del objeto db:
-  db.create_all()
-  
-  Usamos tambien las siguientes funciones de acuerdo a la accion:
-  
-  Para insertar los registros
-  db.session.add(objeto del model (un objeto libro en esta app))
-  
-  Para eliminar los registros
-  db.session.delete(objeto del model (un objeto libro en esta app))
-  
-  Para llamar todos los registros de libro (libros.qury.all() en nuestro ejemplo)
-  model.query.all() 
-  
-  Pdemos igual usar filtros, por ejemplo
-  libros.query.filter_by(author = 'Elvis Poin').all()
+  Si el metodo es post, es decir un formulario es enviado como post
   """
+  if request.method == 'POST':
+    if not request.form['name'] or not request.form['author'] or not request.form['number_pgs']:
+      """
+      Usamos flash para generar un mensaje de error
+      """
+      flash('Por favor agregue todos los campos', 'error')
+    else:
+      libro = libros(request.form['name'],request.form['author'],request.form['number_pgs'])
+      """
+      libro es el objeto de tipo libros creado usando la clase libros 
+      """
+      db.session.add(libro)
+      db.session.commit()
+      flash('Libro creado')
+      return redirect(url_for('show_all'))
+  Return render_template('new.html')
+      
   
-  """
-  Ahora a continuacion pasamos a disenar las views
-  """
-  
-  @app_db.route('/')
-  def show_all():
-    return render_template('show_all.html', libros = libros.query.all())
