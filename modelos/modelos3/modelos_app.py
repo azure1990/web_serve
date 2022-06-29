6fef5fee1579df8e8e7a6c44b33df7060fabf556
Robs <57124267+azure1990@users.noreply.github.com>
2022-06-28 18:21:47 -0600

    Update modelos_app.py

--- modelos/modelos3/modelos_app.py
+++ modelos/modelos3/modelos_app.py
@@ -7,72 +7,67 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///losmodelos.sqlite3'
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///losmodelos3.sqlite3'
 app.secret_key = "0256ac1229d70c6056e9ca7d40a014ae4ff730cac62ba81f"
 db = SQLAlchemy(app)
 
-class Amo(db.Model):
+class Comensal(db.Model):
 
 	id = db.Column('id', db.Integer, primary_key = True)
 	nombre = db.Column(db.String(100))
-	perros = db.relationship('Perro', backref='amo', lazy=True)
-
+	comedores = 
 
 	def __init__(self, nombre):
 		self.nombre = nombre
 
-class Perro(db.Model):
+class Comedor(db.Model):
   
   	id = db.Column('id', db.Integer, primary_key = True, unique = True)
   	nombre = db.Column(db.String(100))
-  	edad = db.Column(db.Integer)
-  	amo_id = db.Column(db.Integer, db.ForeignKey('amo.id'), nullable=False)
+  	comensales =
 
-  	def __init__(self, nombre, edad, amo_id):
+  	def __init__(self, nombre):
 
-  		self.nombre = nombre
-  		self.edad = edad
-  		self.amo_id=amo_id
+  		self.nombre = nombre  		
 	
 
 
 @app.route('/')
-def mostrar_amos():
-  	return render_template('amos.html', amos = Amo.query.all())
+def mostrar_comedores():
+  	return render_template('comedores.html', amos = Comedor.query.all())
+
+@app.route('/')
+def mostrar_comensales():
+  	return render_template('comensales.html', amos = Comensal.query.all())
 
 
-@app.route('/nuevo_amo', methods=['GET', 'POST'])
-def nuevo_amo():
+@app.route('/nuevo_comedor', methods=['GET', 'POST'])
+def nuevo_comedor():
 	if request.method == 'POST':
 		if not request.form['nombre']:
-			flash('Por favor agrega el nombre del nuevo amo', 'error')
+			flash('Por favor agrega el nombre del nuevo comedors', 'error')
 		else:
-			amo = Amo(request.form['nombre'])
-			db.session.add(amo)
+			comedor = Comedor(request.form['nombre'])
+			db.session.add(comedor)
 			db.session.commit()
-			flash('Amo creado')
+			flash('Comedor creado')
 			return redirect('/')  
 			#print(request.form['nombre'])
 			#return request.form['nombre']
-	return render_template('crear_amo.html')
+	return render_template('crear_comedor.html')
 
-
-
-@app.route('/perro/<int:amo_id>', methods=['GET', 'POST'])
-def crear_perro(amo_id):
+@app.route('/nuevo_comedor', methods=['GET', 'POST'])
+def nuevo_comensal():
 	if request.method == 'POST':
-		if not request.form['nombre'] or not request.form['edad']:
-			flash('Por favor llena todo los campos', 'error')
+		if not request.form['nombre']:
+			flash('Por favor agrega el nombre del nuevo comensal', 'error')
 		else:
-			perro = Perro(request.form['nombre'],request.form['edad'], amo_id)
-			db.session.add(perro)
+			comensal = Comensal(request.form['nombre'])
+			db.session.add(comedor)
 			db.session.commit()
-			flash('perro creado')
+			flash('Comensal creado')
 			return redirect('/')  
 			#print(request.form['nombre'])
 			#return request.form['nombre']
-	return render_template('nuevo_perro.html', amo = amo_id)
+	return render_template('crear_comensal.html')
 
-@app.route('/perros/<int:amo>')
-def perros(amo):
-	return render_template('perros.html', perros = Perro.query.filter_by(amo_id = amo).all())
